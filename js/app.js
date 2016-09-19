65ac2e8aa623533e6a4783882b44252db3fe1d58
kskonecka <kasia.skonecka@gmail.com>
2016-09-19 19:50:01 +0200

    horizontal and parallx nested sliders

--- js/app.js
+++ js/app.js
@@ -1,16 +1,37 @@
 $(function(){
   console.log("works");
 
-  var mySwiper = new Swiper ('.swiper-container', {
+  var mySwiper = new Swiper ('#main-swiper', {
+    keyboardControl: true,
     direction: 'vertical',
     speed: 400,
-    keyboardControl: true,
     loop: true,
-    pagination: '.swiper-pagination',
+    pagination: '.swiper-pagination-v',
     paginationType: 'progress',
     paginationHide: false,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    scrollBar: "swiper-scrollbar"
+    paginationClickable: true,
+    // slidesPerView: 'auto',
+    centeredSlides: true,
+    // spaceBetween: 10,
+    parallax: true
   });
+
+  var swiperV = new Swiper('.swiper-container-h', {
+    pagination: '.swiper-pagination-h',
+    paginationClickable: true,
+    loop: true,
+    effect: "fade",
+    keyboardControl:true
+});
+
+var swiperP = new Swiper('.swiper-container-p', {
+    pagination: '.swiper-pagination-p',
+    paginationClickable: true,
+    // nextButton: '.swiper-button-next',
+    // prevButton: '.swiper-button-prev',
+    parallax: true,
+    speed: 600,
+    keyboardControl:true
+});
+
 });
